b511e978e877769607876be3ed15fdbcf052a8df
Grif_on <91139654+grif-on@users.noreply.github.com>
2026-02-23 23:39:10 +0300

    Formatted dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,6 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include <windows.h>
+
 #include "stdio.h"
 
 // #############################################################################################
@@ -7,19 +8,15 @@
 // We don't really use this, but you can if it helps......
 //
 // #############################################################################################
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved) {
+  switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+      break;
+  }
+  return TRUE;
 }
-
